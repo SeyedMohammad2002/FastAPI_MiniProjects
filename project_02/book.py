2fa8c5746db432fc3a1de6680241a6f1e91e6360
SeyedMohammad Nasiry <seyedmohammad.nasiry@gmail.com>
2024-06-12 14:13:58 +0330

    feat: Add new functions to create, update and filter books and write doctype for them.

--- project_02/book.py
+++ project_02/book.py
@@ -33,10 +33,29 @@ class BookRequest(BaseModel):
                 "title": "A new book",
                 "author": "Coding With Ruby",
                 "description": "A new description of a book",
-                "rating": 5                
+                "rating": 5,
             }
         }
- 
+
+
+def set_book_id(book: Book):
+    """find last book id in the BOOKS list
+
+    Args:
+        book (Book): Book object
+
+    Returns:
+        Book object
+    """
+
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    # if len(BOOKS) > 0:
+    #     book.id = BOOKS[-1].id + 1
+    # else:
+    #     book.id = 1
+
+    return book
+
 
 BOOKS = [
     Book(1, "Computer Science Pro", "CodeWithRuby", "A very nice book!", 5),
@@ -50,21 +69,68 @@ BOOKS = [
 
 @app.get("/books")
 async def read_all_books():
+    """return all books from BOOKS
+
+    Returns:
+        list: list of Book objects that shows as dictionary in the result.
+    """
     return BOOKS
+
+
+@app.get("/books/{book_id}")
+async def read_book_with_id(book_id: int):
+    """return book base on id
+
+    Args:
+        book_id (int)
+
+    Returns:
+        Book object
+    """
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+
+
+@app.get("/books/")
+async def read_book_with_rating(book_rating: int):
+    """return book base on specific rating
+
+    Args:
+        book_rating (int)
+
+    Returns:
+        list: list of books that has same rating.
+    """
+    book_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            book_to_return.append(book)
+
+    return book_to_return
+
+
+@app.put("/books/book_update")
+async def update_book(book_request: BookRequest):
+    """update existing book base on book id
+
+    Args:
+        book_request (BookRequest): modified book
+    """
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_request.id:
+            BOOKS[i] = book_request
+            return {"message": "Book was updated"}
 
 
 @app.post("/create-book")
 async def add_new_books(book_request: BookRequest):
+    """create and add new book to BOOKS list
+
+    Args:
+        book_request (BookRequest)
+    """
     new_book = Book(**book_request.model_dump())
     BOOKS.append(set_book_id(new_book))
 
-
-def set_book_id(book: Book):
-    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
-
-    # if len(BOOKS) > 0:
-    #     book.id = BOOKS[-1].id + 1
-    # else:
-    #     book.id = 1
-
-    return book
+    return {"message": "Book added"}
